0a1419d29e97b09d117a9317fb41e45c3a65e5f3
savya sachi <135540082+yachi4123@users.noreply.github.com>
2026-06-18 05:46:37 +0530

    Add: gas-station - submission-4

---
+++ Data Structures & Algorithms/gas-station/submission-4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        if (accumulate(gas.begin(), gas.end(), 0) <
+            accumulate(cost.begin(), cost.end(), 0)) {
+            return -1;
+        }
+
+        int total = 0;
+        int res = 0;
+        for (int i = 0; i < gas.size(); i++) {
+            total += (gas[i] - cost[i]);
+
+            if (total < 0) {
+                total = 0;
+                res = i + 1;
+            }
+        }
+
+        return res;
+    }
+};
